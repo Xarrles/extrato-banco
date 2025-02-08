800fb88692f58c108d8883a5c3229e9aafba8d34
Xarrles <igorxarles@gmail.com>
2025-02-08 09:55:30 -0300

    feat: leitura extrato

---
+++ app/Modules/extrado.py
@@ -0,0 +1,64 @@
+import os
+import re
+from Config.loguru    import logger
+from Libs.loguru      import logger_class
+import ofxparse
+
+
+@logger_class()
+class Extrato:
+
+    def __init__(self):
+        self.path_ofx = r".\entrada\08022025\Extrato conta corrente - 022025.ofx"
+
+
+
+    def fix_ofx_fitid(self, ofx_file):
+        """ Adiciona um FITID único para transações que estejam sem esse campo. """
+        with open(ofx_file, "r", encoding="utf-8") as file:
+            ofx_content = file.read()
+
+        # Encontrar transações sem FITID e adicionar um identificador único
+        fixed_content = re.sub(
+            r"<STMTTRN>(.*?)<FITID>\s*</FITID>",
+            lambda match: f"{match.group(1)}<FITID>{hash(match.group(1))}</FITID>",
+            ofx_content,
+            flags=re.DOTALL
+        )
+
+        # Criar um novo arquivo corrigido
+        name_file = os.path.basename(ofx_file)
+        fixed_file = os.path.join(os.path.dirname(ofx_file), "fixed_" + name_file)
+        with open(fixed_file, "w", encoding="utf-8") as file:
+            file.write(fixed_content)
+
+        return fixed_file
+
+
+    def extrair(self):
+
+        fixed_ofx = self.fix_ofx_fitid(self.path_ofx)
+
+        with open(fixed_ofx, "r", encoding="ISO-8859-1") as file:
+            ofx = ofxparse.OfxParser.parse(file)
+
+        # Exibir informações da conta
+        print(f"Banco: {ofx.account.institution}")
+        print(f"Conta: {ofx.account.account_id}")
+        print(f"Tipo de Conta: {ofx.account.account_type}")
+        print(f"Moeda: {ofx.account.statement.currency}")
+
+        # Exibir transações
+        print("\nTransações:")
+        for transaction in ofx.account.statement.transactions:
+            print(f"Data: {transaction.date}")
+            print(f"Valor: {transaction.amount}")
+            print(f"Tipo: {transaction.type}")
+            print(f"Descrição: {transaction.memo}")
+            print(f"FITID: {transaction.id}")
+            print("-" * 30)
+
+    def main(self):
+        logger.info("\t1.1 -  Extraindo informações do extrato")
+        self.extrair()
+        ...
--- app/__main__.py
+++ app/__main__.py
@@ -1,10 +1,15 @@
-from Config import logger,show_config
-from Libs.loguru import logger_start
-from Config import *
-
+from Config             import *
+from Config             import logger, show_config
+from Libs.loguru        import logger_start
+from Modules.extrado    import Extrato
 
 @logger_start
 def main():
+
+    extrato = Extrato()
+
+    logger.info("1.0 - Extrai informações do extrato")
+    extrato.main()
     ...
 
 if __name__ ==  "__main__":
